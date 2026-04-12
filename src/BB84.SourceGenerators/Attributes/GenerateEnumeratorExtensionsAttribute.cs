06dba858a5fc92bd111b4da2386f24a3f89094a0
BoBoBaSs84 <73112377+BoBoBaSs84@users.noreply.github.com>
2026-04-12 16:47:03 +0200

    fix: warnings due to code generation removed

--- src/BB84.SourceGenerators/Attributes/GenerateEnumeratorExtensionsAttribute.cs
+++ src/BB84.SourceGenerators/Attributes/GenerateEnumeratorExtensionsAttribute.cs
@@ -3,8 +3,6 @@
 //
 // This source code is licensed under the MIT license found in the
 // LICENSE file in the root directory of this source tree.
-using System.ComponentModel;
-
 namespace BB84.SourceGenerators.Attributes;
 
 /// <summary>
@@ -12,7 +10,7 @@ namespace BB84.SourceGenerators.Attributes;
 /// The generated code will include the following extension methods:
 /// <list type="bullet">
 /// <item>A method to get the name of the enum value.</item>
-/// <item>A method to get the description of the enum value from the <see cref="DescriptionAttribute"/>.</item>
+/// <item>A method to get the description of the enum value from the <see cref="System.ComponentModel.DescriptionAttribute"/>.</item>
 /// <item>A method to get the underlying integer value of the enum value.</item>
 /// <item>A method to get all the enum values as a list.</item>
 /// <item>A method to get all the enum names as a list.</item>
